4555e49d2189aa3920960e8f250910655ecbeace
carina-maleski <carinamaleski@gmail.com>
2023-12-15 16:01:32 -0300

    add menu principal com conta corrente e poupança

--- Menu.java
+++ Menu.java
@@ -2,6 +2,8 @@ package conta;
 
 import java.util.Scanner;
 import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
@@ -16,6 +18,21 @@ public class Menu {
 		c1.depositar(5000.0f);
 		c1.visualizar();
 		
+		//Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(2,231,1,"Mariana",15000.0f,1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+		//Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(3,123,2,"Victor",100000.0f,15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 		
 		Scanner leia = new Scanner(System.in);
 

68ea34fded88f0b5381cb122a9a63aba039656e6
carina-maleski <carinamaleski@gmail.com>
2023-12-20 17:26:28 -0300

    add metodos crud

--- controller/ContaController.java
+++ controller/ContaController.java
@@ -12,8 +12,12 @@ public class ContaController implements ContaRepository {
 	
 	@Override
 	public void procurarPorNumero(int numero) {
-		// TODO Auto-generated method stub
+		var conta = buscarNaCollection(numero);
 		
+		if (conta != null)
+			conta.visualizar();
+		else
+			System.out.println("\nA conta número: "+numero+" não foi encontrada!");
 	}
 
 	@Override
@@ -31,13 +35,26 @@ public class ContaController implements ContaRepository {
 
 	@Override
 	public void atualizar(Conta conta) {
-		// TODO Auto-generated method stub
+		var buscaConta = buscarNaCollection(conta.getNumero());
+		
+		if (buscaConta != null) {
+			listaContas.set(listaContas.indexOf(buscaConta), conta);
+			System.out.println("\nA conta número: "+conta.getNumero() + " foi atualizada com sucesso!");
+		} else {
+			System.out.println("\nA conta número: "+conta.getNumero() + " não foi encontrada!");
+		}
 		
 	}
 
 	@Override
 	public void deletar(int numero) {
-		// TODO Auto-generated method stub
+		var conta = buscarNaCollection(numero);
+		
+		if (conta != null) {
+			if(listaContas.remove(conta) == true)
+				System.out.println("\nA Conta numero: "+ numero +" foi deletada com sucesso!");
+		}else
+			System.out.println("\nA Conta numero: "+ numero +" não foi encontrada!");
 		
 	}
 
@@ -61,5 +78,14 @@ public class ContaController implements ContaRepository {
 	
 	public int gerarNumero() {
 		return ++ numero;
+	}
+	
+	public Conta buscarNaCollection(int numero) {
+		for(var conta:listaContas) {
+			if (conta.getNumero()== numero) {
+				return conta;
+			}
+		}
+		return null;
 	}
 }
